668755c85920ae29273a4c9c9ba2b84cdda7c1e9
Vaibhav Agarwal <vaibhav20325@gmail.com>
2023-08-24 11:28:03 +0530

    debugged body-web

--- src/routes/api/body-web.ts
+++ src/routes/api/body-web.ts
@@ -1,14 +1,13 @@
 import * as express from 'express';
-import auth from '../../middleware/auth';
 import {getAllBodies, getBody} from '../../controllers/body-web';
 // import {checkAccessBody} from '../../middleware/checkAccess';
 
 const router = express.Router();
 
 //? Tested OK
-router.get('/', auth, getAllBodies);
+router.get('/', getAllBodies);
 
 //? Tested OK
-router.get('/:id', auth, getBody);
+router.get('/:id', getBody);
 
 export default router;
